735084d586126e09754bb5d052fade5ff97a6cba
Sebastien <seb@dsebastien.net>
2015-12-16 23:27:03 +0100

    Added basic layout, form and icons

--- app/core/directives/registerMaterialDesignLiteElement.ts
+++ app/core/directives/registerMaterialDesignLiteElement.ts
@@ -1,13 +1,13 @@
 import { Directive, ElementRef } from "angular2/angular2";
 
-declare var componentHandler: any; // this is necessary to let TypeScript know that the variable exists already
+declare var componentHandler:any; // this is necessary to let TypeScript know that the variable exists already
 
 @Directive({
-	selector: '[mdl-register-element]'
+	selector: "[mdl-register-element]"
 })
 export class RegisterMaterialDesignLiteElement {
-	constructor(elementRef: ElementRef){
-		const htmlElement: HTMLElement = elementRef.nativeElement;
+	constructor(elementRef:ElementRef) {
+		const htmlElement:HTMLElement = elementRef.nativeElement;
 
 		console.log("Upgrading!");
 		componentHandler.upgradeElement(htmlElement);
