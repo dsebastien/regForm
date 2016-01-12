1d91826f6c5e09eac6ee70fb0e36d12f81ae9978
Sebastien <seb@dsebastien.net>
2016-01-12 18:47:53 +0100

    Updated (temp)

--- app/core/services/api/apiService.ts
+++ app/core/services/api/apiService.ts
@@ -10,7 +10,7 @@ import {TokenDetails} from "./tokenDetails";
 import {Injectable} from "angular2/core";
 
 import {Http, Response, RequestMethod, RequestOptions} from "angular2/http";
-import {Observable} from "rxjs";
+import {Observable, Subject} from "rxjs";
 import "rxjs/add/operator/map";
 
 //TODO remove and re-import localForage once a solution is found for #41: https://github.com/dsebastien/regForm/issues/41
@@ -26,48 +26,55 @@ export class ApiService {
 	private static HEADER_NAME:string = "Authorization";
 	private static HEADER_PREFIX:string = "Bearer";
 	private static LOCALSTORAGE_TOKEN_VARIABLE:string = "id_token";
-	private currentTokenDetails:TokenDetails<string> = new TokenDetails<string>();
+	private currentTokenDetails:TokenDetails<string>;
 
 	constructor(http:Http) {
 		console.log("Loading the API service");
 		this.http = http;
-		
+
+		if (http === null) {
+			throw new Error("The HTTP service has not been provided but is mandatory!");
+		}
+
 		this.initializeToken();
-		
-		//TODO validate that http was actually received
-		// todo use localForage to retrieve the token if present
-		// if present, check validity before requesting a new one
-		this.requestToken();
 		//localForage.setItem("key", "cool", (err, result) => { alert(result.value)});
 		//console.log("Saved!");
 
 		//localForage.clear();
 		//FIXME check that items can be read back
 	}
-	
-	private initializeToken(){
+
+	private initializeToken() {
 		localForage.getItem(ApiService.LOCALSTORAGE_TOKEN_VARIABLE)
-			.then((value) => {
-				console.log("Found: ", value);
-			}, (error: Error) => { console.error("Error while checking for token presence in LocalStorage")});
+			.then((value:string) => {
+				if (value === null) {
+					// fixme in this case we need to request a new token
+					// (saved automatically in this class)
+					// put it here in localstorage
+				} else {
+					console.log("Found: ", value);
+				}
+			}, (error:Error) => {
+				console.error("Error while checking for token presence in LocalStorage")
+			});
 	}
 
-	private requestToken() {
+	private requestToken():Observable<any> {
+		const retVal: Subject<TokenDetails> = new Subject<TokenDetails>();
 		console.log("Requesting token...");
-		// todo if there is already a token in local storage check if it is still valid
-		// if it is then do nothing
-
 		const lastTokenRequestTime = Date.now();
 		this.http.get(Configuration.tokenGenerationEndpoint)
 			.map(res => res.json())
 			.subscribe(tokenAsJSON => {
-				this.currentTokenDetails.token = tokenAsJSON.token;
-				this.currentTokenDetails.expirationTime = lastTokenRequestTime + (tokenAsJSON.expiresIn * 1000); // seconds to milliseconds
-				//console.log(this.lastTokenRequestTime);
-				//console.log(new Date(this.lastTokenRequestTime));
-				//console.log(this.lastTokenExpirationTime);
-				console.log("The token will expire on: " + new Date(this.currentTokenDetails.expirationTime));
+				const tokenDetails = new TokenDetails<string>();
+				tokenDetails.token = tokenAsJSON.token;
+				tokenDetails.expirationTime = lastTokenRequestTime + (tokenAsJSON.expiresIn * 1000); // seconds to milliseconds
+				//console.log("Token requested on ",new Date(this.lastTokenRequestTime));
+				console.log("The token will expire on: " + new Date(tokenDetails.expirationTime));
+				this.currentTokenDetails = tokenDetails;
+				retVal.next(tokenDetails);
 			});
+		return retVal;
 	}
 
 	/*
@@ -76,6 +83,10 @@ export class ApiService {
 	 * @returns {boolean} true if the token is still valid
 	 */
 	isTokenStillValid() {
-		return Date.now() < this.currentTokenDetails.expirationTime;
+		let retVal:boolean = false;
+		if (this.currentTokenDetails !== null) {
+			return retVal = Date.now() < this.currentTokenDetails.expirationTime;
+		}
+		return retVal;
 	}
 }
