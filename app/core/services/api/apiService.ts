ca3764e2c7b6dba0724268dbf0ee4c5264225fd2
Sebastien <seb@dsebastien.net>
2016-01-12 06:35:43 +0100

    Updated api service

--- app/core/services/api/apiService.ts
+++ app/core/services/api/apiService.ts
@@ -14,7 +14,7 @@ import {Observable} from "rxjs";
 import "rxjs/add/operator/map";
 
 //TODO remove and re-import localForage once a solution is found for #41: https://github.com/dsebastien/regForm/issues/41
-const localForage = require("localforage");
+const localForage:LocalForage = require("localforage");
 
 /*
  * Service responsible for requesting/checking tokens.
@@ -23,22 +23,33 @@ const localForage = require("localforage");
 @Injectable()
 export class ApiService {
 	private http:Http;
-	private static headerName:string = "Authorization";
-	private static headerPrefix:string = "Bearer";
-	private static localStorageTokenVariableName:string = "id_token";
+	private static HEADER_NAME:string = "Authorization";
+	private static HEADER_PREFIX:string = "Bearer";
+	private static LOCALSTORAGE_TOKEN_VARIABLE:string = "id_token";
 	private currentTokenDetails:TokenDetails<string> = new TokenDetails<string>();
 
 	constructor(http:Http) {
 		console.log("Loading the API service");
 		this.http = http;
+		
+		this.initializeToken();
+		
 		//TODO validate that http was actually received
 		// todo use localForage to retrieve the token if present
 		// if present, check validity before requesting a new one
 		this.requestToken();
-		localForage.setItem("key", "cool");
-		console.log("Saved!");
+		//localForage.setItem("key", "cool", (err, result) => { alert(result.value)});
+		//console.log("Saved!");
+
 		//localForage.clear();
 		//FIXME check that items can be read back
+	}
+	
+	private initializeToken(){
+		localForage.getItem(ApiService.LOCALSTORAGE_TOKEN_VARIABLE)
+			.then((value) => {
+				console.log("Found: ", value);
+			}, (error: Error) => { console.error("Error while checking for token presence in LocalStorage")});
 	}
 
 	private requestToken() {
