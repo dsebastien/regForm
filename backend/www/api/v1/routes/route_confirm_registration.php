5c03043f7e1afa35631ad2555ba5fbf26526457d
Sebastien <seb@dsebastien.net>
2016-01-18 20:44:32 +0100

    Confirm registration implementation (partial)

--- backend/www/api/v1/routes/route_confirm_registration.php
+++ backend/www/api/v1/routes/route_confirm_registration.php
@@ -16,7 +16,38 @@ $app->get('/confirm_registration/{uuid}', function($request, $response, $args) {
 	// check the database to see if it is known
 	$escapedUuid = $dbConnection->real_escape_string($uuid);
 	
-	// TODO implement
+	$dbConnection = getDatabaseConnection();
+	
+	$registrationExists = false;
+	$registrationAlreadyConfirmed = false;
+	
+	$sql = "SELECT confirmed FROM `foire_vetements` WHERE uuid = '$escapedUuid'";
+	
+	$result = null;
+	if(!$result = $dbConnection->query($sql)){
+		throw new Exception("There was an error running the query. Error: ".$dbConnection->error);
+	}
+	
+	while($row = $result->fetch_assoc()){
+		$registrationExists = true;
+		$registrationAlreadyConfirmed = $row['confirmed'];
+	}
+	$result->free();
+    	
+	if($registrationExists){
+		$response->write("Registration exists!");
+	}
+	
+	if($registrationAlreadyConfirmed){
+		$response->write("\nRegistration already confirmed!");
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 	return $response;
 })->setName("confirm_registration");
