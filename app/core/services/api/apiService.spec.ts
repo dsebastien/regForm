5eaad78ad437dc1c8592d1d05ace72cba04235f8
Sebastien <seb@dsebastien.net>
2016-02-13 12:00:19 +0100

    Fix unit tests (fixes #54)

--- app/core/services/api/apiService.spec.ts
+++ app/core/services/api/apiService.spec.ts
@@ -4,8 +4,6 @@
 // https://github.com/typings/typings/issues/69
 // https://github.com/dsebastien/modernWebDevBuild/issues/76
 
-import {describe, it, expect} from "jasmine";
-
 describe("API Service", () => {
 	it("should", () => {
 		expect(true).toBe(true);
--- app/test/sanity_test.spec.ts
+++ app/test/sanity_test.spec.ts
@@ -4,8 +4,6 @@
 // https://github.com/typings/typings/issues/69
 // https://github.com/dsebastien/modernWebDevBuild/issues/76
 
-import {describe, xdescribe, it, xit, expect} from "jasmine";
-
 describe("sanity checks", () => {
 	it("should be able to test", () => {
 		expect(true).toBe(true);
